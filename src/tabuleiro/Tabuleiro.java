fccd5cd25416f77b30a814d1ebbd3dca3b0e44fc
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-04-20 15:08:26 -0300

    Criando classe de excessões para o tabuleiro

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -7,6 +7,11 @@ public class Tabuleiro {
 	private Peca[][] pecas;// qual peça está em cada posição do tabuleiro
 
 	public Tabuleiro(int linhas, int colunas) {
+		if(linhas < 1  || colunas <1) {
+			throw new excecaoTabuleiro("Erro ao criar tabuleiro,"
+					+ " é necessário que exista ao menos uma linha e uma "
+					+ "coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -16,19 +21,14 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExiste(linha, coluna)) {
+			throw new excecaoTabuleiro("Posição não existe");
+		}
 		return pecas[linha][coluna];
 	}
 
@@ -37,7 +37,24 @@ public class Tabuleiro {
 	}
 
 	public void colocarPeca(Peca peca, Posicao posicao) {
+		if(temPeca(posicao)) {
+			throw new excecaoTabuleiro("Já existe uma peça na posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+	
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < this.linhas && coluna >= 0 && coluna < this.colunas;
+	}
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean temPeca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new excecaoTabuleiro("Posição não existe");
+		}
+		return peca(posicao) != null;
 	}
 }
---
+++ src/tabuleiro/excecaoTabuleiro.java
@@ -0,0 +1,14 @@
+package tabuleiro;
+
+public class excecaoTabuleiro extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public excecaoTabuleiro(String msg) {
+		super(msg);
+	}
+
+}
